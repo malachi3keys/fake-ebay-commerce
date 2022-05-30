d8084c25ed94213f788ca6fc1f9294140a43d68b
malachi3keys <bhibbler.pltw@gmail.com>
2022-05-30 13:56:52 -0400

    first commit

---
+++ auctions/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Bid, Comment, Listing, Tag, User,  Watchlist
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("post_title", "bid_start")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("listing", "user", "body")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "listing", "bidder", "dollar")
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Watchlist)
+admin.site.register(Tag)
---
+++ auctions/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Tag(models.Model):
+    post_tag = models.CharField(max_length=20)
+        
+    def __str__(self):
+        return f"{self.post_tag}"
+        
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="postuser")
+    post_title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    bid_start = models.DecimalField(max_digits=10, decimal_places=2)
+    # img = models.ImageField
+    img = models.URLField(max_length=200, blank=True)
+    list_tag = models.ManyToManyField(Tag, related_name="category")
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.post_title}"
+
+
+class Bid(models.Model):
+    dollar = models.DecimalField(max_digits=10, decimal_places=2)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_listing")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+
+    def __str__(self):
+        return f"{self.dollar}"
+
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="replyuser")
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.listing}"
+        
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watcher")
+    listing = models.ManyToManyField(Listing, blank=True, related_name="watchlisting")
+
+    def __str__(self):
+        return f"{self.user}"
